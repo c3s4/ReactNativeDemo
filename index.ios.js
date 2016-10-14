3dc8fc84458e60bad19035542f892c75967fea1c
Davide Cesaroni <davide.cesaroni@gmail.com>
2016-10-14 15:39:27 +0200

    Implement basic stuff based on tutorial

--- index.ios.js
+++ index.ios.js
@@ -12,20 +12,66 @@ import {
   View
 } from 'react-native';
 
-export default class ReactNativeDemo extends Component {
+class ReactNativeDemo extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.titleContainer}>
+          <Text style={styles.mainTitle}>
+            The Main Screen
+          </Text>
+        </View>
+        <View style={styles.elementsContainer}>
+          <View style={styles.elementsLeftColumn}>
+            <FileBox title="ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone ciaone " />
+            <FileBox />
+            <FileBox />
+          </View>
+          <View style={styles.elementsRightColumn}>
+            <FileBox />
+            <FileBox />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+class Greetings extends Component {
+  render() {
+    return (
+      <Text style={this.props.style}>
+        Hi, {this.props.name}
+      </Text>
+    );
+  }
+}
+
+class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{flex: 1}}>
+        <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
+        <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
+        <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
+      </View>
+    );
+  }
+}
+
+class FileBox extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.fileBox}>
+        <Text>{this.props.title}</Text>
       </View>
     );
   }
@@ -34,20 +80,61 @@ export default class ReactNativeDemo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
+  },
+  titleContainer: {
+    backgroundColor: '#555555',
+    padding: 20
+  },
+  elementsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  elementsLeftColumn: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5
+  },
+  elementsRightColumn: {
+    flex: 1,
+    flexDirection: 'column',
+    marginRight: 5,
+    marginTop: 5
+  },
+  fileBox: {
+    minHeight: 80,
+    backgroundColor: '#888888',
+    marginBottom: 5
+  },
+  hidden: {
+    opacity: 0
+  },
+  mainTitle: {
+    fontSize: 30,
+    color: '#FFFFFF',
+    textAlign: 'center'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 15
+  },
+  greeting: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('ReactNativeDemo', () => ReactNativeDemo);
